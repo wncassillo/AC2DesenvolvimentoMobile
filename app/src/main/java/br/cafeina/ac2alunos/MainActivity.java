4d60db49c932be8c2105c0dc35a479cc0e366ffd
wncassillo <cassilloneto@gmail.com>
2024-05-28 03:51:18 -0300

    2nd commit, tela adicionarAluno criada

--- app/src/main/java/br/cafeina/ac2alunos/MainActivity.java
+++ app/src/main/java/br/cafeina/ac2alunos/MainActivity.java
@@ -1,11 +1,15 @@
 package br.cafeina.ac2alunos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,18 +26,26 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private AlunoAdapter alunoAdapter;
     private List<Aluno> alunoList = new ArrayList<>();
+    private FloatingActionButton buttonAddAluno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonAddAluno = findViewById(R.id.ButtonAddAluno);
         recyclerView = findViewById(R.id.recyclerViewAlunos);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         alunoAdapter = new AlunoAdapter(alunoList);
         recyclerView.setAdapter(alunoAdapter);
 
         loadData();
+
+        buttonAddAluno.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddActivity.class);
+            startActivity(intent);
+        });
+
     }
 
     private void loadData() {
